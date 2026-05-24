78d8057e6ca35060f100332b4e5c6a9ec1d5f356
kerog-dev <286162490+kerog-dev@users.noreply.github.com>
2026-05-24 17:03:57 +0300

    fix Snap

--- src/apps/Snap.tsx
+++ src/apps/Snap.tsx
@@ -1,15 +1,15 @@
-// import snapHtml from "../assets/snap.html?raw";
+import snapHtml from "../assets/snap.html?raw";
 
 export default function Snap() {
-  // const blob = new Blob([snapHtml], { type: "text/html" });
-  // const url = URL.createObjectURL(blob);
-  return <p>Out of order!</p>;
-  // return (
-  //   <iframe
-  //     src={url}
-  //     title="Snap!"
-  //     width={window.innerWidth}
-  //     height={window.innerHeight}
-  //   ></iframe>
-  // );
+  const blob = new Blob([snapHtml], { type: "text/html" });
+  const url = URL.createObjectURL(blob);
+  return (
+    <iframe
+      src={url}
+      title="Snap!"
+      width={window.innerWidth}
+      height={window.innerHeight}
+      sandbox="allow-scripts allow-same-origin allow-camera allow-microphone"
+    ></iframe>
+  );
 }
