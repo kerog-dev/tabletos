9b2e48db5c95191707dee14dbe34dea070dc8381
kerog-dev <286162490+kerog-dev@users.noreply.github.com>
2026-06-21 05:04:51 +0300

    shortcuts: display grid instead of custom positioning

--- src/components/wm/Shortcuts.tsx
+++ src/components/wm/Shortcuts.tsx
@@ -7,22 +7,14 @@ interface ShortcutDesc {
   name: string;
   target: App;
   iconFile: string | null;
-  pos: [number, number];
 }
 
-// TODO: display: grid; instead of this current thing.
 function Shortcut({ s, spawnWindow }: { s: ShortcutDesc; spawnWindow: (app: App) => void }) {
   const src = s.iconFile ? useBlobFileUrl(s.iconFile) : undefined;
 
   return (
     <div
       className="shortcut"
-      style={{
-        top: s.pos[1] + "px",
-        left: s.pos[0] + "px",
-        width: SHORTCUT_SIZE + "px",
-        height: SHORTCUT_SIZE + "px",
-      }}
       onDoubleClick={() => spawnWindow(s.target)}
     >
       <div className="shortcut-inside">
@@ -33,20 +25,10 @@ function Shortcut({ s, spawnWindow }: { s: ShortcutDesc; spawnWindow: (app: App)
   );
 }
 
-const SHORTCUT_SIZE = 100;
-
 export function Shortcuts({ spawnWindow }: { spawnWindow: (app: App) => void }) {
   const apps = useApps();
   const shortcuts = useMemo<ShortcutDesc[]>(() => {
-    return apps.map((app, i) => {
-      const pos: [number, number] = [i * SHORTCUT_SIZE, 0];
-      while (pos[0] >= innerWidth - SHORTCUT_SIZE) {
-        pos[0] -= innerWidth;
-        pos[0] = Math.max(0, pos[0]);
-        pos[1] += SHORTCUT_SIZE;
-      }
-      return ({ name: app.name, target: app, iconFile: "/wallpaper.img", pos });
-    });
+    return apps.map(app => ({ name: app.name, target: app, iconFile: "/wallpaper.img" }));
   }, [apps]);
 
   return (
