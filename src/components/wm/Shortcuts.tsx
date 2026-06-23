aed9d1dd77dce12f20b48e2eb23a9bc28161f695
kerog-dev <286162490+kerog-dev@users.noreply.github.com>
2026-06-23 20:28:31 +0300

    additional shortcut system, support multiple shortcut types such as script

--- src/components/wm/Shortcuts.tsx
+++ src/components/wm/Shortcuts.tsx
@@ -1,21 +1,71 @@
 import { useMemo } from "react";
-import { type App, useApps } from "../../apps.ts";
-import { useBlobFileUrl } from "../../fs.ts";
+import { type App, apps, useApps } from "../../apps.ts";
+import { readTextFile, useBlobFileUrl } from "../../fs.ts";
 import "./Shortcuts.css";
+import { toast, Urgency } from "../../toast.tsx";
 
-interface ShortcutDesc {
+interface ShortcutShared {
   name: string;
-  target: App;
   iconFile: string | null;
 }
+
+interface AppTarget {
+  targetType: "app";
+  app: App | string;
+}
+
+interface ScriptTarget {
+  targetType: "script";
+  isPath: boolean;
+  script: string;
+}
+
+type ShortcutDesc = ShortcutShared & (AppTarget | ScriptTarget);
+
+async function loadAdditionalShortcuts(): Promise<ShortcutDesc[]> {
+  try {
+    const shortcuts = JSON.parse(await readTextFile("/additional_shortcuts.json"));
+    return shortcuts;
+  } catch {
+    return [];
+  }
+}
+
+const additionalShortcuts = await loadAdditionalShortcuts();
 
 function Shortcut({ s, spawnWindow }: { s: ShortcutDesc; spawnWindow: (app: App) => void }) {
   const src = s.iconFile ? useBlobFileUrl(s.iconFile) : undefined;
 
+  function open() {
+    switch (s.targetType) {
+      case "app":
+        const app: App | undefined = typeof s.app === "string" ? apps.find(app => app.name === s.app) : s.app;
+        if (app) spawnWindow(app);
+        else toast({ title: "Failed to open, app not found", urgency: Urgency.Error });
+        break;
+
+      case "script":
+        function runScript(script: string) {
+          try {
+            window.eval(script);
+          } catch (e) {
+            toast({ title: "Error running script", desc: "Error: " + e, urgency: Urgency.Error });
+          }
+        }
+
+        if (s.isPath) {
+          readTextFile(s.script).then(script => {
+            runScript(script);
+          });
+        } else runScript(s.script);
+        break;
+    }
+  }
+
   return (
     <div
       className="shortcut"
-      onDoubleClick={() => spawnWindow(s.target)}
+      onDoubleClick={() => open()}
     >
       <div className="shortcut-inside">
         {src && <img className="shortcut-icon" src={src} />}
@@ -25,11 +75,19 @@ function Shortcut({ s, spawnWindow }: { s: ShortcutDesc; spawnWindow: (app: App)
   );
 }
 
+function appsToShortcuts(apps: App[]): ShortcutDesc[] {
+  return apps.map(app => ({ name: app.name, targetType: "app", app, iconFile: "/wallpaper.img" }));
+}
+
 export function Shortcuts({ spawnWindow }: { spawnWindow: (app: App) => void }) {
   const apps = useApps();
-  const shortcuts = useMemo<ShortcutDesc[]>(() => {
-    return apps.map(app => ({ name: app.name, target: app, iconFile: "/wallpaper.img" }));
-  }, [apps]);
+  const shortcuts = useMemo<ShortcutDesc[]>(
+    () => [
+      ...appsToShortcuts(apps),
+      ...additionalShortcuts,
+    ],
+    [apps],
+  );
 
   return (
     <div className="shortcuts">
