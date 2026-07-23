d52852344182e1dbce22b4dce14628bfada20571
kerog-dev <286162490+kerog-dev@users.noreply.github.com>
2026-07-23 21:41:25 +0300

    moving booting logic to boot.ts

---
+++ src/boot.ts
@@ -0,0 +1,63 @@
+import React from "react";
+import * as JSXRuntime from "react/jsx-runtime";
+
+export async function boot() {
+  const unlistenEarlyErrorListeners = (() => {
+    const errorListener = (e: ErrorEvent) => {
+      if (e.error instanceof Error) {
+        alert(`Error: ${e.error.name}: ${e.error.message}\n${e.error.stack ?? "No stack"}`);
+      } else {
+        alert(`Error: ${e.error}`);
+      }
+    };
+    const unhandledRejectionListener = (e: PromiseRejectionEvent) => {
+      alert(`Promise rejection: ${e.reason}`);
+    };
+
+    window.addEventListener("error", errorListener);
+    window.addEventListener("unhandledrejection", unhandledRejectionListener);
+
+    return () => {
+      window.removeEventListener("error", errorListener);
+      window.removeEventListener("unhandledrejection", unhandledRejectionListener);
+    };
+  })();
+
+  Object.assign(window as any, {
+    $ready: new Promise(res => (window as any).$resolve = res),
+  });
+
+  // TODO: show boot progress
+  // TODO: verify no toplevel side effects and do toplevel import
+  const { eventlog, EventUrgency } = await import("./eventlog.ts");
+  await import("./sdk.ts");
+  const { loadPackages } = await import("./loader/loader.ts");
+  const { toast, Urgency } = await import("./toast.tsx");
+
+  Object.assign(window as any, {
+    __React: React,
+    __ReactJsxRuntime: JSXRuntime,
+  });
+
+  window.addEventListener("error", (e) => {
+    try {
+      eventlog.add("Errors", "Uncaught error", EventUrgency.Error, "Error: " + String(e.error));
+      toast({ title: "Error", desc: String(e.error), urgency: Urgency.Error });
+    } catch {
+      alert(String(e.error));
+    }
+  });
+
+  window.addEventListener("unhandledrejection", (e) => {
+    try {
+      eventlog.add("Errors", "Unhandled rejection", EventUrgency.Error, "Reason: " + String(e.reason));
+      toast({ title: "Unhandled rejection", desc: String(e.reason), urgency: Urgency.Error });
+    } catch {
+      alert(String(e.reason));
+    }
+  });
+
+  unlistenEarlyErrorListeners();
+
+  loadPackages();
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,14 @@
-import { unlisten } from "./earlyinit.ts";
-import { eventlog, EventUrgency } from "./eventlog.ts";
-import "./sdk.ts";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import React from "react";
-import * as JSXRuntime from "react/jsx-runtime";
 import favicon from "./assets/favicon.svg";
-import WindowManager from "./components/wm/WindowManager.tsx";
-import { loadPackages } from "./loader/loader.ts";
-import { toast, Urgency } from "./toast.tsx";
+import { boot } from "./boot.ts";
 
-Object.assign(window as any, {
-  __React: React,
-  __ReactJsxRuntime: JSXRuntime,
+await boot().catch(() => {
+  // TODO: show the error, and show a recovery screen
 });
 
-window.addEventListener("error", (e) => {
-  try {
-    eventlog.add("Errors", "Uncaught error", EventUrgency.Error, "Error: " + String(e.error));
-    toast({ title: "Error", desc: String(e.error), urgency: Urgency.Error });
-  } catch {
-    alert(String(e.error));
-  }
-});
-
-window.addEventListener("unhandledrejection", (e) => {
-  try {
-    eventlog.add("Errors", "Unhandled rejection", EventUrgency.Error, "Reason: " + String(e.reason));
-    toast({ title: "Unhandled rejection", desc: String(e.reason), urgency: Urgency.Error });
-  } catch {
-    alert(String(e.reason));
-  }
-});
-
-unlisten();
-
-loadPackages();
+const { default: WindowManager } = await import("./components/wm/WindowManager.tsx");
 
 const faviconEl = document.createElement("link");
 faviconEl.rel = "icon";
